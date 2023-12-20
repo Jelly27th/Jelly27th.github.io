adf34d5efbbd31158da9833c8e0730fb97ae2885
Jelly27th <872908391@qq.com>
2023-12-20 19:50:52 +0800

    Site updated: 2023-12-20 19:50:51

--- js/duration.js
+++ js/duration.js
@@ -1,3 +1,3 @@
-// build time:Fri Oct 20 2023 23:19:31 GMT+0800 (China Standard Time)
+// build time:Wed Dec 20 2023 19:50:35 GMT+0800 (China Standard Time)
 !function(){var n=new Date("2022/01/22 00:00:00");function m(){var m=new Date;m.setTime(m.getTime()+250);days=(m-n)/1e3/60/60/24;dnum=Math.floor(days);hours=(m-n)/1e3/60/60-24*dnum;hnum=Math.floor(hours);if(String(hnum).length===1){hnum="0"+hnum}minutes=(m-n)/1e3/60-24*60*dnum-60*hnum;mnum=Math.floor(minutes);if(String(mnum).length===1){mnum="0"+mnum}seconds=(m-n)/1e3-24*60*60*dnum-60*60*hnum-60*mnum;snum=Math.round(seconds);if(String(snum).length===1){snum="0"+snum}document.getElementById("timeDate").innerHTML="本站安全运行&nbsp"+dnum+"&nbsp天";document.getElementById("times").innerHTML=hnum+"&nbsp小时&nbsp"+mnum+"&nbsp分&nbsp"+snum+"&nbsp秒"}m();setInterval(m,1e3)}();
 //rebuild by hrmmi 
